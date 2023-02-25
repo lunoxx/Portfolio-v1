dde04d3690f03e6c9a780cc93f5d2fe01d891fec
lunoxx <a.mese99@gmail.com>
2023-02-25 17:55:22 +0200

    Animated navbar for mobile devices

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -1,13 +1,43 @@
 
 
+var mobile_menu_opened = false;
 
+$(document).ready(function() 
+{
+    $(document).mouseup(function(e){
+        
+        if(mobile_menu_opened) 
+        {
+            var menu = $('#nav_panel');
+            var toggleButton = $('#toggleButton');
 
-var toggleNavBar = () => {
-    var panel = document.getElementById("nav_panel");
+            if (!menu.is(e.target) && !toggleButton.is(e.target) && menu.has(e.target).length === 0)
+            {
+                toggleMenu();
+            }
+        }
+    });
+});
 
-    if (panel.style.display === "block") {
-        panel.style.display = "none";
-    } else {
-        panel.style.display = "block";
+var toggleMenu = () => {
+
+    var panel = $('#nav_panel');
+
+    panel.toggleClass("active");
+    $('body').toggleClass("fixed-position");
+
+    mobile_menu_opened = !mobile_menu_opened;
+
+    if(mobile_menu_opened) { // show
+        panel.show();
+        panel.removeClass("out").addClass("active");
+    }
+    else { // hide
+        panel.removeClass("active").addClass("out");
+
+        setTimeout(function() {
+            panel.hide();
+            
+        }, 300); // same time as animation
     }
 }
