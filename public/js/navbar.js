946663abe8b4418e5aa3eb53fe1e30c10d3f3ecf
lunoxx <a.mese99@gmail.com>
2023-03-05 17:31:04 +0200

    Production version

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -1,1 +1,41 @@
-var mobile_menu_opened=!1;$(document).ready(function(){$(document).mouseup(function(a){if(mobile_menu_opened){var o=$("#nav_panel"),n=$("#toggleButton");o.is(a.target)||n.is(a.target)||0!==o.has(a.target).length||e()}});var e=()=>{var e=$("#nav_panel");e.toggleClass("active"),$("body").toggleClass("fixed-position"),(mobile_menu_opened=!mobile_menu_opened)?(e.show(),e.removeClass("out").addClass("active")):(e.removeClass("active").addClass("out"),setTimeout(function(){e.hide()},301))}});
+var mobile_menu_opened = false;
+
+$(document).ready(function() 
+{
+    $(document).mouseup(function(e){
+        
+        if(mobile_menu_opened) 
+        {
+            var menu = $('#nav_panel');
+            var toggleButton = $('#toggleButton');
+
+            if (!menu.is(e.target) && !toggleButton.is(e.target) && menu.has(e.target).length === 0)
+            {
+                toggleMenu();
+            }
+        }
+    });
+
+});
+var toggleMenu = () => {
+
+    var panel = $('#nav_panel');
+
+    panel.toggleClass("active");
+    $('body').toggleClass("fixed-position");
+
+    mobile_menu_opened = !mobile_menu_opened;
+
+    if(mobile_menu_opened) { // show
+        panel.show();
+        panel.removeClass("out").addClass("active");
+    }
+    else { // hide
+        panel.removeClass("active").addClass("out");
+
+        setTimeout(function() {
+            panel.hide();
+            
+        }, 300); // same time as animation
+    }
+}
